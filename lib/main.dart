e0ecfbf073ff28ac2e54b621263232cc7ca72ede
chhoung <phengchhoung@gmail.com>
2020-02-17 16:43:46 +0700

    initial version

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(QuizApp());
+
+class QuizApp extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('HELLO WORLD')
+        ),
+      ),
+    );
+  }
+
+}
