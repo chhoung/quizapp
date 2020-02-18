0e853c570e49385f520a2777cef39cb157f22842
chhoung <phengchhoung@gmail.com>
2020-02-18 16:20:00 +0700

    add screens to app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_analytics/observer.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:quizapp/screens/screens.dart';
 
 void main() => runApp(QuizApp());
 
@@ -8,23 +11,37 @@ class QuizApp extends StatelessWidget{
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
-      home: Scaffold(
-        body: new AwesomePage(),
-      ),
+      navigatorObservers: [
+          FirebaseAnalyticsObserver(analytics: FirebaseAnalytics()),
+      ],
+
+      //name routes
+      routes: {
+        '/' : (context) => LoginScreen(),
+        '/topics': (context) => TopicsScreen(),
+        '/profile': (context) => ProfileScreen(),
+        '/about': (context) => AboutScreen(),
+
+      },
+
+      //Theme
+       theme: ThemeData(
+          fontFamily: 'Nunito',
+          bottomAppBarTheme: BottomAppBarTheme(
+            color: Colors.black87,
+          ),
+          brightness: Brightness.dark,
+          textTheme: TextTheme(
+            body1: TextStyle(fontSize: 18),
+            body2: TextStyle(fontSize: 16),
+            button: TextStyle(letterSpacing: 1.5, fontWeight: FontWeight.bold),
+            headline: TextStyle(fontWeight: FontWeight.bold),
+            subhead: TextStyle(color: Colors.grey),
+          ),
+          buttonTheme: ButtonThemeData(),
+        ),
     );
   }
 
 }
 
-class AwesomePage extends StatelessWidget {
-  const AwesomePage({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Text('HELLO WORLD'),
-    );
-  }
-}
---
+++ lib/screens/login.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+// import '../shared/shared.dart';
+// import '../services/services.dart';
+
+class LoginScreen extends StatefulWidget {
+  LoginScreen({Key key}) : super(key: key);
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       
+    );
+  }
+}
