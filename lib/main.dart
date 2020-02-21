e4e1b557278da979787d082b87b0a3e334ec3bfe
chhoung <phengchhoung@gmail.com>
2020-02-21 13:56:09 +0700

    deleting comments

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,6 @@ void main() => runApp(QuizApp());
 class QuizApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return MultiProvider(
       providers: [
         StreamProvider<Report>.value(
--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -8,7 +8,6 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.deepOrange,
